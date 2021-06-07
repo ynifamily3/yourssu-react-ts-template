562bdcb9ae2a47c8476351fae8c7f750a7282317
Jongkeun Choi <jongkeun.ch@gmail.com>
2021-06-07 22:53:34 +0900

    fix path

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { hot } from "react-hot-loader/root";
 import React, { useEffect } from "react";
 import styles from "App.module.css";
 import imgtest from "assets/logo_2020.png";
-import { TIMEOUT } from "./@api/rest/a";
+import { TIMEOUT } from "@api/rest";
 function App(): JSX.Element {
   useEffect(() => {
     console.log(TIMEOUT);
